419682cec6c201bee0871890374b7fb7854860dd
David Arinder <34454124+davidArinder@users.noreply.github.com>
2020-12-04 16:43:36 -0500

    more challenges

---
+++ day4/practice.js
@@ -0,0 +1,28 @@
+// const string = "150cm"
+
+// const slice = string.slice(0,3)
+
+// if (string.slice(0,3) >= 150) {
+//     console.log("true")
+// }
+
+// const second = "#18171d"
+
+// // const check = /[0-9a-f]/.test(hcl)
+
+// if (second.slice(0,1) == "#" && second.length == 7 && /[0-9a-f]/.test(second.slice(1,7))) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+const second = "01234567ef"
+
+if (isNaN(second) == false && second.length == 9) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+
+
